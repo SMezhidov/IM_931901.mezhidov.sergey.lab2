a882fea16fcd2e6b4e9e75aed802d987b2cdfdb4
SMezhidov <63232091+SMezhidov@users.noreply.github.com>
2022-05-21 10:45:43 +0300

    Add files via upload

---
+++ IM_lab2/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace IM_lab2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.edEuro = new System.Windows.Forms.NumericUpDown();
+            this.edDol = new System.Windows.Forms.NumericUpDown();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.edEuro)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.edDol)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Курс евро";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(251, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(99, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Курс доллара";
+            // 
+            // edEuro
+            // 
+            this.edEuro.DecimalPlaces = 2;
+            this.edEuro.Location = new System.Drawing.Point(104, 9);
+            this.edEuro.Name = "edEuro";
+            this.edEuro.Size = new System.Drawing.Size(120, 22);
+            this.edEuro.TabIndex = 2;
+            this.edEuro.Value = new decimal(new int[] {
+            7023,
+            0,
+            0,
+            131072});
+            // 
+            // edDol
+            // 
+            this.edDol.DecimalPlaces = 2;
+            this.edDol.Location = new System.Drawing.Point(365, 9);
+            this.edDol.Name = "edDol";
+            this.edDol.Size = new System.Drawing.Size(120, 22);
+            this.edDol.TabIndex = 3;
+            this.edDol.Value = new decimal(new int[] {
+            6712,
+            0,
+            0,
+            131072});
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(504, 9);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 4;
+            this.btnStart.Text = "Старт";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.BtnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(599, 9);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 5;
+            this.btnStop.Text = "Стоп";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.BtnStop_Click);
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(0, 53);
+            this.chart1.Name = "chart1";
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series3.IsValueShownAsLabel = true;
+            series3.LabelForeColor = System.Drawing.SystemColors.MenuHighlight;
+            series3.LabelFormat = "C";
+            series3.Legend = "Legend1";
+            series3.Name = "Курс Евро";
+            series3.YValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Double;
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series4.IsValueShownAsLabel = true;
+            series4.LabelForeColor = System.Drawing.Color.Tomato;
+            series4.LabelFormat = "C";
+            series4.Legend = "Legend1";
+            series4.Name = "Курс Доллара";
+            series4.YValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Double;
+            this.chart1.Series.Add(series3);
+            this.chart1.Series.Add(series4);
+            this.chart1.Size = new System.Drawing.Size(1108, 504);
+            this.chart1.TabIndex = 6;
+            this.chart1.Text = "chart1";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1108, 560);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.edDol);
+            this.Controls.Add(this.edEuro);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Lab2";
+            ((System.ComponentModel.ISupportInitialize)(this.edEuro)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.edDol)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown edEuro;
+        private System.Windows.Forms.NumericUpDown edDol;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ IM_lab2/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IM_lab2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        const double k = 0.05;
+        double rateE, rateD;
+        int day = 0;
+
+        Random random = new Random();
+
+        private void BtnStop_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+
+        
+
+        private void BtnStart_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+            rateE = (double)edEuro.Value;
+            rateD = (double)edDol.Value;
+
+            chart1.Series[0].Points.Clear();
+            chart1.Series[0].Points.AddXY(0, rateE);
+
+            chart1.Series[1].Points.Clear();
+            chart1.Series[1].Points.AddXY(0, rateD);
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            rateE = rateE * (1 + k * (random.NextDouble() - 0.5));
+            chart1.Series[0].Points.AddXY(day++, rateE);
+
+            rateD = rateE * (1 + k * (random.NextDouble() - 0.5));
+            chart1.Series[1].Points.AddXY(day, rateD);
+        }
+    }
+}
